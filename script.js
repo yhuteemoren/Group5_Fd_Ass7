7e79d2153dacab4d18855b4a4182f042c6022424
HeemahMuslad <rohimat.m1800276@st.futminna.edu.ng>
2024-05-20 14:48:30 +0100

    added a change

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const fetchData = async ()=>{
     try{
 const response = await fetch("https://dog.ceo/api/breed/hound/images");
 const data = await response.json();
-data.message.forEach((url)=>{
+const limitedData = data.message.slice(0, 50);
+limitedData.forEach((url)=>{
     const image = document.createElement("li");
     image.innerHTML = `<div class="imgContainer">
     <img src=${url} alt="dog">
